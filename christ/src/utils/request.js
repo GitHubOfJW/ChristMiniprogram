8d2a97b8158c9ccdda26d634dc674144e4010a9d
zhujianwei <zhujiawei_web@126.com>
2019-05-17 23:31:58 +0800

    列表刷新

--- christ/src/utils/request.js
+++ christ/src/utils/request.js
@@ -2,7 +2,8 @@ import wepy from 'wepy'
 
 export default class Request {
   static request(params) {
-    params.url = 'http://192.168.2.106:3000' + params.url
+    // params.url = 'http://192.168.2.106:3000' + params.url
+    params.url = 'http://localhost:3000' + params.url
     return wepy.request(params)
   }
 }
